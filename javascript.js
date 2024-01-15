42bef3aa3a23bbea51c9459083c58ec4765346c1
Aomsup Pongpan <dollars2869@gmail.com>
2024-01-15 19:46:02 +0700

    Add popup-content

--- javascript.js
+++ javascript.js
@@ -1,55 +1,31 @@
 
-let cartClicked = true;
-const img = document.querySelector(".product-image");
-const title = document.querySelector(".title")
-const color = document.querySelector(".color")
-const circleOrange = document.querySelector(".circle-orange")
-const circleBlue = document.querySelector(".circle-blue")
+const img = document.querySelector(".popup-product-image");
+const circleOrange = document.querySelector(".popup-circle-orange");
+const circleBlue = document.querySelector(".popup-circle-blue");
 const addCart = document.querySelector(".add-to-cart");
 const cartAdded = document.querySelector(".cart-added");
-const star = document.querySelector(".star");
+
+const contentNike = document.querySelector(".content-nike");
+const contentPopup = document.querySelector(".content-popup");
+
+function circleColorFirst () {
+    console.log(1);
+    img.src = "./img/metcon-9-amp-orange.png";
+    addCart.style.backgroundColor = "orange";
+    addCart.style.color = "black";
+    console.log(img);
+}
+
+function circleColorSecond () {
+    console.log(2);
+    img.src = "./img/metcon-9-amp-blue.png";
+    addCart.style.backgroundColor = "blue";
+    addCart.style.color = "white";
+    console.log(img);
+}
 
 
-
-
-circleOrange.addEventListener("click", ()=>{
+contentNike.addEventListener("click",()=>{
+    contentPopup.style.display = "block";
     
-    if(cartClicked){
-        title.style.color = "orange";
-        color.innerHTML = "Color : Orange";
-        img.src ="./img/metcon-9-amp-Orange.png"
-        addCart.style.backgroundColor = "orange";
-        addCart.style.color = "black"
-        star.style.color = "orange"
-        
-    }
- 
-
 })
-
-circleBlue.addEventListener("click", ()=>{
-
-    if(cartClicked){
-        title.style.color = "blue";
-        color.innerHTML = "Color : Blue";
-        img.src ="./img/metcon-9-amp-blue.png"
-        addCart.style.backgroundColor = "blue"
-        addCart.style.color = "white"
-        star.style.color = "blue"
-        
-        
-    }
-   
-  
-})
-
-addCart.addEventListener("click", ()=>{
-
-    if(cartClicked){
-        addCart.style.display = "none";
-        cartAdded.style.display = "block";
-        cartClicked = false;
-        
-    }
-    
-});
